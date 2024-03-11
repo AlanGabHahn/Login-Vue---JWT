bf7f040744113d45d0e97437e7a869da707d2f12
Alan <alan_hahn1995@outlook.com>
2024-03-11 09:09:35 -0300

    Projeto de login com vue e laravel

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <router-link :to="{name: 'home'}">Home</router-link>
+  <router-link :to="{name: 'login'}">Login</router-link>
+  <router-link :to="{name: 'dashboard'}">Dashboard</router-link>
+
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script setup>
 
-nav {
-  padding: 30px;
-}
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<style lang="scss" scoped>
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
