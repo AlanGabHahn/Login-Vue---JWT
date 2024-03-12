a08e0d21cc2c6de1a833f37c4cba272053d6bf4d
Alan <alan_hahn1995@outlook.com>
2024-03-12 08:58:57 -0300

    Ajustes no front e criado a controller de auth

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
-<template>
-  <router-link :to="{name: 'home'}">Home</router-link>
-  <router-link :to="{name: 'login'}">Login</router-link>
-  <router-link :to="{name: 'dashboard'}">Dashboard</router-link>
-
+<template> 
+<ul>
+  <li>
+    <router-link :to="{name: 'home'}">Home</router-link>
+  </li>
+  <li>
+    <router-link :to="{name: 'login'}">Login</router-link>
+  </li>
+  <li>
+    <router-link :to="{name: 'dashboard'}">Dashboard</router-link>
+  </li>
+</ul>
   <router-view/>
 </template>
 
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,29 @@
 <template >
-  <div>
-    
-  </div>
+  <h2>Login</h2>
+  <form @submit.prevent="login">
+    <input type="text" placeholder="Seu Email" v-model="user.email">
+    <input type="password" placeholder="Sua Senha" v-model="user.password">
+    <button type="submit">Login</button>
+  </form>
 </template>
 
 <script setup>
+  import http from '@/services/http.js';
+  import {reactive} from 'vue';
 
+  const user = reactive({
+    email: '',
+    password: ''
+  });
+
+  async function login() {
+    try {
+      const {data} = await http.post('/auth', user);
+      console.log(data);
+    } catch (error) {
+      console.log(error?.response?.data);
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
